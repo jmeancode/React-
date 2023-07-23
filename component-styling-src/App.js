e237226a421f729c0e2861a240421b50a3fa0480
jmean <seongjimin199@gmail.com>
2023-07-23 15:15:05 +0900

    Add files via upload

---
+++ component-styling-src/App.js
@@ -0,0 +1,15 @@
+import ScssComponent from "./components/ScssComponent";
+import CssModule from "./components/CssModule";
+import StyledComponent from "./components/StyledComponent";
+
+function App() {
+  return (
+    <>
+      {/* <ScssComponent></ScssComponent> */}
+      {/* <CssModule></CssModule> */}
+      <StyledComponent></StyledComponent>
+    </>
+  );
+}
+
+export default App;
